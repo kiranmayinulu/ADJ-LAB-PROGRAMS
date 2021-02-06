3a570fb14a458b6288a4b57b47e032ced6b37321
kiranmayinulu <78654568+kiranmayinulu@users.noreply.github.com>
2021-02-06 21:44:13 +0530

    EXPERIMENT-1

---
+++ Array/Hashtableexample.java
@@ -0,0 +1,15 @@
+package Array;
+import java.util.Hashtable;
+import java.util.Map;
+public class Hashtableexample{
+	public static void main(String args[]){
+		Hashtable<Integer,String> hm=new Hashtable<Integer,String>();
+		hm.put(100,"Amit");
+		hm.put(102,"Ravi");
+		hm.put(101,"Vijay");
+		hm.put(103,"Rahul");
+		for(Map.Entry m:hm.entrySet()){
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+	}
+}
